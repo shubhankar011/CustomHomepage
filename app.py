d09317a671fa16221be0259c0d3cfd3aeb0f47a2
Shubhankar <shubhankarpandey2007@outlook.com>
2026-01-22 17:29:32 +0530

    Half completed

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+@app.route('/settings')
+def settings_page():
+    return render_template('settings.html')
+if __name__ == "__main__":
+    app.run(debug=True)
