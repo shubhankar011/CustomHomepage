ff35f02e2475deaa76d1b094e67ad5b142def9c1
Shubhankar Kumar <162623205+shubhankar011@users.noreply.github.com>
2026-03-02 23:21:09 +0530

    Simplify Flask app initialization
    Simplified Flask app initialization by removing custom template and static folder settings.

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, 
-            template_folder=os.path.join(os.path.dirname(__file__), 'templates'),
-            static_folder=os.path.dirname(__file__),
-            static_url_path=''
-           )
+app = Flask(__name__)
 
 @app.route('/')
 def home_page():
