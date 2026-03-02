850e331accc58928242f8257c6db7b3a75857e30
Shubhankar Kumar <162623205+shubhankar011@users.noreply.github.com>
2026-03-02 23:19:21 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='.', static_url_path='')
+app = Flask(__name__,
+                template_folder='templates',
+                static_folder='.',
+                static_url_path=''
+            )
 
 @app.route('/')
 def home_page():
